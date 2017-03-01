f7b139881e7f60aaacf2211d079ae1c5369ec928
berwyn <berwyn.codeweaver@gmail.com>
2017-02-28 18:30:35 -0700

    feat(worker): Parse topics page

--- auracite-worker/src/main.rs
+++ auracite-worker/src/main.rs
@@ -5,11 +5,11 @@ use std::io::{Read};
 
 use hyper::{Client};
 use select::document::Document;
-use select::predicate::{Class, Name};
+use select::predicate::{Class};
 
 fn main() {
     println!("Hello, Worker!");
-    let homepage = download_page("http://na.finalfantasyxiv.com/lodestone/");
+    let homepage = download_page("http://na.finalfantasyxiv.com/lodestone/topics/");
     parse_document(homepage.as_str());
 }
 
@@ -24,7 +24,10 @@ fn download_page(url: &str) -> String {
 fn parse_document(doc: &str) {
     let document = Document::from(doc);
     for topic in document.find(Class("news__content__list__topics")).children().iter() {
-        let title = topic.find(Class("ic_topics")).children().first().unwrap().text();
-        println!("{:?}", title);
+        let title = topic.find(Class("ic_topics")).children().first();
+        match title {
+            Some(node) => println!("{:?}", node.text()),
+            None => {},
+        }
     }
 }
