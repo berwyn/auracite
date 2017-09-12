42eb7d3d5ab5a0a1a631510d4eba89b476996fa3
berwyn <berwyn.codeweaver@gmail.com>
2017-09-11 21:20:48 -0400

    refactor(web): Add language handling

--- src/lodestone/routes.rs
+++ src/lodestone/routes.rs
@@ -1,35 +1,57 @@
 use jsonfeed;
 use jsonfeed::Feed;
+use rocket::http::RawStr;
 use rss;
 use rss::ChannelBuilder;
 use storage;
 
-#[get("/rss")]
-pub fn rss() -> String {
+fn lang_accepted(lang: &String) -> bool {
+    lang == "na"
+        || lang == "de"
+        || lang == "jp"
+        || lang == "fr"
+}
+
+#[get("/<lang>/rss")]
+pub fn rss(lang: &RawStr) -> Option<String> {
+    let parsed = lang.to_lowercase();
+    if !lang_accepted(&parsed) {
+        return None;
+    }
+
     let conn = storage::connect_redis().unwrap();
-    let items: Vec<rss::Item> = storage::pull_news("na", &conn)
+    let items: Vec<rss::Item> = storage::pull_news(&lang.to_lowercase(), &conn)
         .unwrap()
         .iter()
         .map(|i| i.to_rss())
         .collect();
 
-    ChannelBuilder::default()
+    let content = ChannelBuilder::default()
         .title("FINAL FANTASY XIV The Lodestone")
         .link("https://na.finalfantasyxiv.com/lodestone/")
         .items(items)
         .build()
         .unwrap()
-        .to_string()
+        .to_string();
+    
+    Some(content)
 }
 
-#[get("/jsonfeed")]
-pub fn jsonfeed() -> String {
+#[get("/<lang>/jsonfeed")]
+pub fn jsonfeed(lang: &RawStr) -> Option<String> {
+    let parsed = lang.to_lowercase();
+    if !lang_accepted(&parsed) {
+        return None;
+    }
+
     let mut feed = Feed::builder().title("FINAL FANTASY XIV The Lodestone");
     let conn = storage::connect_redis().unwrap();
-    let items = storage::pull_news("na", &conn).unwrap();
+    let items = storage::pull_news(&lang.to_lowercase(), &conn).unwrap();
     for item in items {
         feed = feed.item(item.to_jsonfeed());
     }
 
-    jsonfeed::to_string(&feed.build()).unwrap()
+    let content = jsonfeed::to_string(&feed.build()).unwrap();
+
+    Some(content)
 }
--- src/storage.rs
+++ src/storage.rs
@@ -3,6 +3,8 @@ use lodestone::NewsItem;
 use web::feed::FeedItem;
 use redis::{Client, Commands, Connection, RedisError};
 
+const NEWS_KEY: &'static str = "auracite:news";
+
 pub fn connect_redis() -> Result<Connection, RedisError> {
     let env = load_var("REDIS_URL", "redis://127.0.0.1:6379");
     let url = env.as_str();
@@ -17,13 +19,13 @@ pub fn push_news(locale: &'static str,
                  -> Result<(), RedisError> {
     let json: Vec<String> = topics.iter().map(|ref t| t.to_json()).collect();
 
-    conn.rpush(format!("news:{}", locale), json)
+    conn.rpush(format!("{}:{}", NEWS_KEY, locale), json)
 }
 
-pub fn pull_news(locale: &'static str,
+pub fn pull_news(locale: &String,
                  conn: &Connection)
                  -> Result<Vec<Box<FeedItem>>, RedisError> {
-    let vec: Vec<String> = conn.lrange(format!("news:{}", locale), -20, -1)?;
+    let vec: Vec<String> = conn.lrange(format!("{}:{}", NEWS_KEY, locale), -20, -1)?;
 
     Ok(vec.iter().map(|s| Box::new(NewsItem::from_json(s)) as Box<FeedItem>).collect())
 }
